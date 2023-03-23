42bb55d6012b7326608e5dd604a35b596fa7bace
DersuG <dersu.goorskey@gmail.com>
2023-03-23 15:15:08 -0400

    add remaining data fields

--- custom/data.h
+++ custom/data.h
@@ -1,18 +1,33 @@
 static struct {
 	float timestamp;
 	float altitude;
-} altitude_data_point;
+} barometer_data_point;
 
 static struct {
 	float timestamp;
 	float x;
 	float y;
 	float z;
-} acceleration_1_data_point;
+} accelerometer_1_data_point;
 
 static struct {
 	float timestamp;
 	float x;
 	float y;
 	float z;
-} acceleration_2_data_point;
+} accelerometer_2_data_point;
+
+static struct {
+	float timestamp;
+	float x;
+	float y;
+	float z;
+} magnetometer_data_point;
+
+static struct {
+	float timestamp;
+	float latitude;
+	float longitude;
+	int satellite_count; // unsure if needed
+	float ground_speed; // unsure if needed
+} gps_data_point;
