706e9c090b6445f7b113f8fb476ffa1d177b824d
DersuG <dersu.goorskey@gmail.com>
2023-03-22 21:45:28 -0400

    outline recovery logic

---
+++ custom/recovery.h
@@ -0,0 +1,10 @@
+/*
+ * TODO
+ * - write a queue to hold data points
+ * - write filtering logic
+ * - write recovery decision logic
+ * - write a process function
+ *   - check data.c for newer data points, enqueue if there are any
+ *   - dequeue any data points that are too old
+ *   - also dequeue if there are too many data points
+ */
