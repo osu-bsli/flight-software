c24cf92e3a7fdddd1c26ae4cd1ce873bced29300
DersuG <dersu.goorskey@gmail.com>
2023-03-22 21:42:53 -0400

    add data fields

---
+++ custom/data.h
@@ -0,0 +1,18 @@
+static struct {
+	float timestamp;
+	float altitude;
+} altitude_data_point;
+
+static struct {
+	float timestamp;
+	float x;
+	float y;
+	float z;
+} acceleration_1_data_point;
+
+static struct {
+	float timestamp;
+	float x;
+	float y;
+	float z;
+} acceleration_2_data_point;
