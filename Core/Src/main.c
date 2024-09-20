bcc40a6c52b5a49a45c49627054f0c6661e96e11
Brian Jia <brian.jia04@gmail.com>
2024-09-20 19:03:54 -0400

    Enable SWD

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -202,6 +202,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_OUT_LED_BLUE_Pin|GPIO_OUT_LED_GREEN_Pin|GPIO_OUT_LED_RED_Pin, GPIO_PIN_RESET);
